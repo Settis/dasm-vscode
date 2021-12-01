b26aa460bcb05854bc0c8fc5e373f993cfdbc5ea
Anton <strannik.anton@gmail.com>
2021-12-01 20:28:32 +0100

    Delete diagnostics after file was closed
    Closes: #16

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,5 @@
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import {
-	createConnection, Diagnostic, DiagnosticSeverity, InitializeParams, TextDocuments, TextDocumentSyncKind
-} from 'vscode-languageserver/node';
+import { createConnection, InitializeParams, TextDocuments, TextDocumentSyncKind } from 'vscode-languageserver/node';
 import { parseProgram } from './ast';
 import { validateProgram } from './validators/general';
 
@@ -25,6 +23,10 @@ function validateTextDocument(textDocument: TextDocument) {
 	const diagnostics = validateProgram(programNode);
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
+
+documents.onDidClose(change => {
+	connection.sendDiagnostics({ uri: change.document.uri, diagnostics: []});
+});
 
 documents.listen(connection);
 connection.listen();
