e9cf9b6a8d351b48a2794502a748cbc29525be3b
Anton <strannik.anton@gmail.com>
2022-09-07 22:59:25 +0200

    OK, fine.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -12,8 +12,9 @@ let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	let serverModulePath = path.join('out', 'server.js');
-	if (process.env.COLLECT_COVERAGE === 'true')
+	if (process.env.COLLECT_COVERAGE === 'true') {
 		serverModulePath = path.join('server', 'out', 'serverWithCoverage.js');
+	}
     const serverModule = context.asAbsolutePath(serverModulePath);
     const debugOptions = {execArgv: ['--nolazy', '--inspect=6009', '--enable-source-maps']};
     const serverOptions: ServerOptions = {
