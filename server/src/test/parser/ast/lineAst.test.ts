b2f45b153392475808952431c1c9693cc6820699
Anton <strannik.anton@gmail.com>
2023-01-15 22:44:39 +0100

    Refactoring for Sonar

--- server/src/test/parser/ast/lineAst.test.ts
+++ server/src/test/parser/ast/lineAst.test.ts
@@ -1,5 +1,5 @@
 import { AddressMode, NodeType } from "../../../parser/ast/nodes";
-import { checkAST, TestLineNode } from "./utils";
+import { checkAST, TestArgumentNode, TestCommandNode, TestExpressionNode, TestLabelNode, TestLineNode } from "./utils";
 
 describe('Correct AST for line', () => {
     it('emty text', () => {
@@ -12,356 +12,141 @@ describe('Correct AST for line', () => {
         checkAST('   \n    ');
     });
     it('LABEL                            ', () => {
-        checkAST('LABEL                            ', {
-            type: NodeType.Line,
-            label: {
-                type: NodeType.Label,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LABEL'
-                }
-            },
-            command: null
-        });
+        checkAST('LABEL                            ',
+            aLine('LABEL')
+        );
     });
     it('.LABEL                            ', () => {
-        checkAST('.LABEL                            ', {
-            type: NodeType.Line,
-            label: {
-                type: NodeType.Label,
-                name: {
-                    type: NodeType.Identifier,
-                    name: '.LABEL'
-                }
-            },
-            command: null
-        });
+        checkAST('.LABEL                            ',
+            aLine('.LABEL')
+        );
     });
     it('                        ; Comment', () => {
         checkAST('                        ; Comment');
     });
     it('LABEL                   ; Comment', () => {
-        checkAST('LABEL                   ; Comment', {
-            type: NodeType.Line,
-            label: {
-                type: NodeType.Label,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LABEL'
-                }
-            },
-            command: null
-        });
+        checkAST('LABEL                   ; Comment',
+            aLine('LABEL')
+        );
     });
     it('        CMD ARG,X,ARG2           ', () => {
-        checkAST('        CMD ARG,X,ARG2           ', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'CMD'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.AddressX,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG'
-                        }
-                    },
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG2'
-                        }
-                    }
-                ]
-            }
-        });
+        checkAST('        CMD ARG,X,ARG2           ',
+            aLine(undefined, 'CMD', anArg('ARG', AddressMode.AddressX), anArg('ARG2'))
+        );
     });
     it('LABEL   CMD             ; Comment', () => {
-        checkAST('LABEL   CMD             ; Comment', {
-            type: NodeType.Line,
-            label: {
-                type: NodeType.Label,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LABEL'
-                }
-            },
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'CMD'
-                },
-                args: []
-            }
-        });
+        checkAST('LABEL   CMD             ; Comment',
+            aLine('LABEL', 'CMD')
+        );
     });
     it('LABEL   CMD ARG,X       ; Comment', () => {
-        checkAST('LABEL   CMD ARG,X       ; Comment', {
-            type: NodeType.Line,
-            label: {
-                type: NodeType.Label,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LABEL'
-                }
-            },
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'CMD'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.AddressX,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG'
-                        }
-                    }
-                ]
-            }
-        });
+        checkAST('LABEL   CMD ARG,X       ; Comment',
+            aLine('LABEL', 'CMD', anArg('ARG', AddressMode.AddressX))
+        );
     });
     it('        CMD ARG,  ARG2  ; Comment', () => {
-        checkAST('        CMD ARG,  ARG2  ; Comment', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'CMD'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG'
-                        }
-                    },
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG2'
-                        }
-                    }
-                ]
-            }
-        });
+        checkAST('        CMD ARG,  ARG2  ; Comment',
+            aLine(undefined, 'CMD', anArg('ARG'), anArg('ARG2'))
+        );
     });
     it('LABEL   CMD ARG,X,ARG2  ; Comment', () => {
-        checkAST('LABEL   CMD ARG,X,ARG2  ; Comment', {
-            type: NodeType.Line,
-            label: {
-                type: NodeType.Label,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LABEL'
-                }
-            },
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'CMD'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.AddressX,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG'
-                        }
-                    },
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'ARG2'
-                        }
-                    }
-                ]
-            }
-        });
+        checkAST('LABEL   CMD ARG,X,ARG2  ; Comment',
+            aLine('LABEL', 'CMD', anArg('ARG', AddressMode.AddressX), anArg('ARG2'))
+        );
     });
     it('    INCLUDE "FILE.ASM"', () => {
-        checkAST('    INCLUDE "FILE.ASM"', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'INCLUDE'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.StringLiteral,
-                            text: 'FILE.ASM'
-                        }
-                    }
-                ]
-            }
-        });
+        checkAST('    INCLUDE "FILE.ASM"',
+            aLine(undefined, 'INCLUDE', {
+                type: NodeType.Argument,
+                addressMode: AddressMode.None,
+                value: {
+                    type: NodeType.StringLiteral,
+                    text: 'FILE.ASM'
+                }
+            })
+        );
     });
     // Check address parsing
     it('  LDX #$44', () => {
-        checkAST('  LDX #$44', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LDX'
-                },
-                args: [{
+        checkAST('  LDX #$44',
+            aLine(undefined, 'LDX', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.Immediate,
                     value: {
                         type: NodeType.Number,
                         value: 0x44
                     }
-                }]
-            }
-        });
+                }
+            )
+        );
     });
     it('  LDX ($44,X)', () => {
-        checkAST('  LDX ($44,X)', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LDX'
-                },
-                args: [{
+        checkAST('  LDX ($44,X)',
+            aLine(undefined, 'LDX', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.IndirectX,
                     value: {
                         type: NodeType.Number,
                         value: 0x44
                     }
-                }]
-            }
-        });
+                }
+            )
+        );
     });
     it('  LDX ($44),Y', () => {
-        checkAST('  LDX ($44),Y', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LDX'
-                },
-                args: [{
+        checkAST('  LDX ($44),Y',
+            aLine(undefined, 'LDX', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.IndirectY,
                     value: {
                         type: NodeType.Number,
                         value: 0x44
                     }
-                }]
-            }
-        });
+                }
+            )
+        );
     });
     it('  LDX ($44)', () => {
-        checkAST('  LDX ($44)', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LDX'
-                },
-                args: [{
+        checkAST('  LDX ($44)',
+            aLine(undefined, 'LDX', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.Indirect,
                     value: {
                         type: NodeType.Number,
                         value: 0x44
                     }
-                }]
-            }
-        });
+                }
+            )
+        );
     });
     it('  LDX $44,X', () => {
-        checkAST('  LDX $44,X', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LDX'
-                },
-                args: [{
+        checkAST('  LDX $44,X',
+            aLine(undefined, 'LDX', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.AddressX,
                     value: {
                         type: NodeType.Number,
                         value: 0x44
                     }
-                }]
-            }
-        });
+                }
+            )
+        );
     });
     it('  LDX $44,Y', () => {
-        checkAST('  LDX $44,Y', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'LDX'
-                },
-                args: [{
+        checkAST('  LDX $44,Y', 
+            aLine(undefined, 'LDX', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.AddressY,
                     value: {
                         type: NodeType.Number,
                         value: 0x44
                     }
-                }]
-            }
-        });
+                }
+            )
+        );
     });
     // Check number parsing
-    const NUMBER_LINE: TestLineNode = {
-        type: NodeType.Line,
-        label: null,
-        command: {
-            type: NodeType.Command,
-            name: {
-                type: NodeType.Identifier,
-                name: 'TEST'
-            },
-            args: [
+    const NUMBER_LINE: TestLineNode = aLine(undefined, 'TEST', 
                 {
                     type: NodeType.Argument,
                     addressMode: AddressMode.None,
@@ -370,9 +155,7 @@ describe('Correct AST for line', () => {
                         value: 42
                     }
                 }
-            ]
-        }
-    };
+        );
     it('   TEST %101010', () => {
         checkAST('   TEST %101010', NUMBER_LINE);
     });
@@ -386,105 +169,84 @@ describe('Correct AST for line', () => {
         checkAST('   TEST $2A', NUMBER_LINE);
     });
     it("    dc 'a", () => {
-        checkAST("    dc 'a", {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'dc'
-                },
-                args: [{
+        checkAST("    dc 'a", 
+            aLine(undefined, 'dc', {
                     type: NodeType.Argument,
                     addressMode: AddressMode.None,
                     value: {
                         type: NodeType.CharLiteral,
                         value: 'a'
                     }
-                }] 
-            }
-        });
+                }
+            )
+        );
     });
     it('  dc.s "unicode"', () => {
-        checkAST('  dc.s "unicode"', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'dc'
-                },
-                args: [{
+        checkAST('  dc.s "unicode"', 
+            aLine(undefined, 'dc', {
+                type: NodeType.Argument,
+                addressMode: AddressMode.None,
+                value: {
+                    type: NodeType.StringLiteral,
+                    text: 'unicode'
+                }
+            })
+        );
+    });
+    it('  ECHO "Synonim for . ", *', () => {
+        checkAST('  ECHO "Synonim for . ", *', 
+            aLine(undefined, 'ECHO', 
+                {
                     type: NodeType.Argument,
                     addressMode: AddressMode.None,
                     value: {
                         type: NodeType.StringLiteral,
-                        text: 'unicode'
+                        text: 'Synonim for . '
                     }
-                }] 
-            }
-        });
-    });
-    it('  ECHO "Synonim for . ", *', () => {
-        checkAST('  ECHO "Synonim for . ", *', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'ECHO'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.StringLiteral,
-                            text: 'Synonim for . '
-                        }
-                    }, {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: '*'
-                        }
-                    }
-                ]
-            }
-        });
+                }, 
+                anArg('*')
+            )
+        );
     });
     it('Macros call with two arguments', () => {
-        checkAST('  SOME FIRST, SECOND', {
-            type: NodeType.Line,
-            label: null,
-            command: {
-                type: NodeType.Command,
-                name: {
-                    type: NodeType.Identifier,
-                    name: 'SOME'
-                },
-                args: [
-                    {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'FIRST'
-                        }
-                    }, {
-                        type: NodeType.Argument,
-                        addressMode: AddressMode.None,
-                        value: {
-                            type: NodeType.Identifier,
-                            name: 'SECOND'
-                        }
-                    }
-                ]
-            }
-        });
+        checkAST('  SOME FIRST, SECOND', aLine(undefined, 'SOME', anArg('FIRST'), anArg('SECOND')));
     });
 });
+
+function aLine(labelName?:string, commandName?: string, ...args: TestArgumentNode[]): TestLineNode {
+    let labelNode: TestLabelNode | null = null;
+    if (labelName)
+        labelNode = {
+            type: NodeType.Label,
+            name: {
+                type: NodeType.Identifier,
+                name: labelName
+            }
+        }
+    let commandNode: TestCommandNode | null = null;
+    if (commandName)
+        commandNode = {
+            type: NodeType.Command,
+            name: {
+                type: NodeType.Identifier,
+                name: commandName
+            },
+            args: args
+        }
+    return {
+        type: NodeType.Line,
+        label: labelNode,
+        command: commandNode
+    };
+}
+
+function anArg(name: string, addressMode?: AddressMode): TestArgumentNode {
+    return {
+        type: NodeType.Argument,
+        addressMode: addressMode || AddressMode.None,
+        value: {
+            type: NodeType.Identifier,
+            name: name
+        }
+    }
+}
