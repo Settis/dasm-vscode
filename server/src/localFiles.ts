64cc342437d7f00dda022823049f2184caec2207
Anton <strannik.anton@gmail.com>
2023-02-26 00:22:54 +0100

    Accurate work with URI join

--- server/src/localFiles.ts
+++ server/src/localFiles.ts
@@ -20,6 +20,12 @@ export function readContent(uri: string): string | undefined {
     }
 }
 
+export function joinUri(uri: string, ...paths: string[]): string {
+    const fsPath = URI.parse(uri).fsPath;
+    const fullPath = path.join(fsPath, ...paths);
+    return URI.file(fullPath).toString(true);
+}
+
 export function getFolder(uri: string): string {
     return URI.file(path.parse(URI.parse(uri).fsPath).dir).toString(true)
 }
